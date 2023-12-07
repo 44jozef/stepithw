9567a621e2703e6e6a26748fb65c9ca56a09a1eb
44jozef <135236693+44jozef@users.noreply.github.com>
2023-12-07 17:04:47 +0400

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Classes/SUVFactory.cs
@@ -0,0 +1,32 @@
+﻿using CarFabric.Classes.Cars;
+using CarFabric.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFabric.Classes.CarFactories;
+
+internal class SUVFactory : IFactory
+{
+
+
+    public SUVFactory()
+    {
+
+    }
+    public ICar CreateCar(ICar car)
+    {
+        var Suv = car as SUV;
+
+        Suv.Wheels = 4;
+        Suv.IsOffRoad = false;
+        Suv.Model = "Toyota RAV4";
+        Suv.MaxSpeed = 180;
+        Suv.HP = 222;
+
+        return Suv;
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Classes/TruckFactory.cs
@@ -0,0 +1,29 @@
+﻿using CarFabric.Classes.Cars;
+using CarFabric.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory.Classes.CarFactories;
+
+internal class TruckFactory : IFactory
+{
+    public TruckFactory()
+    {
+
+    }
+
+    public ICar CreateCar(ICar car)
+    {
+        var truck = car as Truck;
+
+        truck.Wheels = 4;
+        truck.Model = "Ford F-150 Raptor";
+        truck.MaxSpeed = 215;
+        truck.HP = 430;
+
+        return truck;
+    }
+}
