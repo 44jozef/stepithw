fdb74dbc3d470b7789801eba613afc826dd4e313
44jozef <135236693+44jozef@users.noreply.github.com>
2024-06-23 13:06:14 +0400

    Add files via upload

---
+++ 04.06.2024/auth/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginForm from './auth';
+import Main from './Main';
+import MainHeader from './MainHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <MainHeader />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
